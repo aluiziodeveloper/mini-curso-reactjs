666bab7662bf8537cdc95f4d03b59eec5236338d
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-11-19 11:16:14 -0300

    Page Create Location - part. 1

--- src/pages/CreateLocation/index.tsx
+++ src/pages/CreateLocation/index.tsx
@@ -1,8 +1,124 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+import { Map, TileLayer, Marker} from 'react-leaflet';
+import api from '../../services/api';
+import logo from '../../assets/logo.svg';
+import './styles.css';
+
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
 
 const CreateLocation: React.FC = () => {
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+    
     return (
-        <h1>Create Location</h1>
+        <div id="page-create-location">
+            <div className="content">
+                <header>
+                    <img src={logo} alt="Coleta Seletiva"/>
+                    <Link to="/">
+                        <FiArrowLeft />
+                        Voltar para home
+                    </Link>
+                </header>
+
+                <form>
+                    <h1>Cadastro do <br /> local de coleta</h1>
+
+                    <fieldset>
+                        <legend>
+                            <h2>Dados</h2>
+                        </legend>
+                        <div className="field">
+                            <label htmlFor="name">Nome da entidade</label>
+                            <input 
+                                type="text"
+                                name="name"
+                                id="name"
+                            />
+                        </div>
+                        <div className="field-group">
+                            <div className="field">
+                                <label htmlFor="email">E-mail</label>
+                                <input 
+                                type="email"
+                                name="email"
+                                id="email"
+                                />
+                            </div>
+                            <div className="field">
+                                <label htmlFor="whatsapp">Whatsapp</label>
+                                <input 
+                                type="text"
+                                name="whatsapp"
+                                id="whatsapp"
+                                />
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>
+                        <h2>Endereço</h2>
+                            <span>Marque o endereço no mapa</span>
+                        </legend>
+                        <Map center={[-23.0003709, -43.365895]} zoom={14}>
+                            <TileLayer
+                                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[-23.0003709, -43.365895]} />
+                        </Map>
+                        <div className="field-group">
+                            <div className="field">
+                                <label htmlFor="city">Cidade</label>
+                                <input 
+                                type="text"
+                                name="city"
+                                id="city"
+                                />
+                            </div>
+                            <div className="field">
+                                <label htmlFor="uf">Estado</label>
+                                <input 
+                                type="text"
+                                name="uf"
+                                id="uf"
+                                />
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>
+                            <h2>Ítens coletados</h2>
+                            <span>Você pode marcar um ou mais ítens</span>
+                        </legend>
+                        <ul className="items-grid">
+                            {items.map(item => (
+                                <li key={item.id}>
+                                    <img src={item.image_url} alt={item.title} />
+                                </li>
+                            ))}
+                        </ul>
+                    </fieldset>
+
+                    <button type="submit">
+                        Cadastrar local de coleta
+                    </button>
+                </form>
+            </div>
+        </div>
     );
 }
 
